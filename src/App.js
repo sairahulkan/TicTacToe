86c7463e134b691dc0c6b8e765cd4ef777fff63f
skandu12 <117236050+skandu12@users.noreply.github.com>
2023-09-19 21:45:17 -0700

    Updated game to include X's

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './styles.css';
 
-function App() {
+function Square(value){
+  
+  return <button className="square">{value}</button>;
+}
+export default function Board() {
+  const[values,setState] = useState(Array(9).fill(null));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="board-row">
+        <Square value = {values[0]}/>
+        <Square value = {values[1]}/>
+        <Square value = {values[2]}/>
+      </div>
+      <div className="board-row">
+        <Square value = {values[3]}/>
+        <Square value = {values[4]}/>
+        <Square value = {values[5]}/>
+      </div>
+      <div className="board-row">
+        <Square value = {values[6]}/>
+        <Square value = {values[7]}/>
+        <Square value = {values[8]}/>
+      </div>
+    </>
   );
 }
-
-export default App;
